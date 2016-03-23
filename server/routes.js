25b449fb9278feb50ea43e58f4a3768aa7e5b4aa
Yashwant Keswani <yashwant.keswani@gmail.com>
2016-03-23 18:34:18 +0530

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -12,6 +12,13 @@ export default function(app) {
   app.use('/api/v1/things', require('./api/thing'));
   app.use('/api/v1/users', require('./api/user'));
   app.use('/api/v1/organizations', require('./api/organization'));
+  app.use('/api/v1/organizationOrganizationRequests', require('./api/organizationOrganizationRequest'));
+  app.use('/api/v1/transactions', require('./api/transaction'));
+  app.use('/api/v1/events', require('./api/event'));
+  app.use('/api/v1/gallerys', require('./api/gallery'));
+  app.use('/api/v1/notifications', require('./api/notification'));
+  app.use('/api/v1/oneTimeTransactions', require('./api/oneTimeTransaction'));
+  app.use('/api/v1/querys', require('./api/query'));
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
