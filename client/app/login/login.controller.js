8a4a3dbab871126096dc42cf1648d000eccb4cfa
Yashodhan Mohan Bhatnagar <yashodhan.mohan@gmail.com>
2016-04-04 21:20:56 +0530

    Login Page Complete

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -6,26 +6,25 @@
         constructor($http, $location) {
             this.$http = $http;
             this.$location = $location;
-            this.error = false;
-            this.errorMessage = "Invalid username and/or password";
             this.username = '';
             this.password = '';
+            this.error = false;
         }
 
         login = () => {
             console.log('Entering login function with values '+this.username+', '+this.password);
             this.$http.post('/api/v1/users/login', { username: this.username, password: this.password })
                 .success((data, status) => {
-                    if (status == 200 && data.status) {
-                        this.$location.path('/');
+                    if (status == 200) {
+                        this.$location.path('/uprofile/'+data.username);
                         this.$scope.disabled = false;
                     } else {
-                        user = false;
+                        this.error = true;
                     }
                 })
                 .error((status) => {
+                    console.log(status);
                     this.error = true;
-                    this.disabled = true;
                 });
         }
     }
@@ -33,30 +32,3 @@
     angular.module('goodbookApp')
         .controller('LoginController', LoginController);
 })();
-
-// 'use strict';
-
-// angular.module('goodbookApp')
-//     .controller('LoginController', ['$scope', '$location', 'AuthService', function($scope, $location, AuthService) {
-//         $scope.login = function() {
-//             //Initial values
-//             $scope.error = false;
-//             $scope.disabled = true;
-
-//             //call login from service
-//             AuthService.login($scope.loginform.username, $scope.loginform.password)
-//                 //handle success
-//                 .then(function() {
-//                     $location.path('/');
-//                     $scope.disabled = false;
-//                 })
-//                 //handle error
-//                 .catch(function() {
-//                     $scope.error = true;
-//                     $scope.errorMessage = "Invalid username and/or password";
-//                     $scope.disabled = true;
-//                     $scope.loginForm = {};
-//                 });
-
-//         };
-//     }]);
