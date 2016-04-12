7ef059e9b6ca38d3993c623382e0e17ecb8cd8dd
Yashodhan Mohan Bhatnagar <yashodhan.mohan@gmail.com>
2016-04-12 16:08:16 +0530

    Procfile added for Heroku.

--- server/api/notification/notification.model.js
+++ server/api/notification/notification.model.js
@@ -1,14 +1,22 @@
 'use strict';
 var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 
+var SingleNotification = new mongoose.Schema({
+    type: Number,
+    nNo: Number,
+    content: String,
+    eventId: String
+});
+
 var NotificationSchema = new mongoose.Schema({
-    userId : String,
+    userId: String,
     /*userId : mongoose.Schema.Types.ObjectId*/
-    organization : Boolean,
-    notifications : [{
-                nNo : Number,
-                content : String
-            }]
- });
+    organization: Boolean,
+    notifications: [SingleNotification]
+});
 
 export default mongoose.model('Notification', NotificationSchema);
+
+// Notification types:
+// 1: Volunteer call
+// 2: Query reply
